4cea51d1aaca46440ee4e6ad09cd710e68eb42bd
animalplay <juandavidgonzalezurrego@gmail.com>
2023-09-05 19:22:52 -0500

    twiter-card

---
+++ first-project/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+export function App() {
+  return (
+    <article>
+      <header>
+        <img src="https://unavatar.io/midudev" alt="El avatar de midudev" />
+        <div>
+          <strong>Miguel Angel Duran</strong>
+          <span>@midudev</span>
+        </div>
+      </header>
+
+      <aside>
+        <button>
+          Seguir
+        </button>
+      </aside>
+    </article>
+  )
+}
